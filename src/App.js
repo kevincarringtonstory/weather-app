26a3a257b446598b1c3624e2291a8e31dacf8d0b
Kevin Carrington <Kevington99@gmail.com>
2024-06-28 11:48:41 -0700

    remove api test log

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
   const [loading, setLoading] = useState(false);
 
   const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-  console.log('API Key:', API_KEY);
 
   useEffect(() => {
     document.title = 'Weather App';
